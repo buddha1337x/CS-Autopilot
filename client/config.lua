a8d39cb498284b1a607fd650c2b9b313eebf0fe4
buddha1337x <karsunog.business@gmail.com>
2025-03-18 22:22:13 -0400

    added notification positioning and timer

---
+++ client/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.AutopilotVehicles = {
+    "adder",
+    "zentorno",
+    "infernus"
+}
+
+-- Options: "top-left", "top-right", "left", "right"
+Config.NotificationPosition = "right"
+
+-- Notification display duration (in milliseconds).
+Config.NotificationTime = 5000  -- 5 seconds
